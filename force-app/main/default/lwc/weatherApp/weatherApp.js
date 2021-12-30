2da6ff5229c3163e8c235b07120dda3d537cbc30
yury trypolski <yury101.89@gmail.com>
2021-12-30 18:46:38 +0300

    added query to server with weather

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,13 +1,37 @@
 import { LightningElement } from 'lwc';
+import getTemperature from '@salesforce/apex/WeatherAppController.getTemperature';
+import getCustomSettingsWeather from '@salesforce/apex/WeatherAppController.getCustomSettingsWeather';
 
 export default class WeatherApp extends LightningElement {
 
+    apiKey;
+    api;
     forecast;
+    city;
+
+    connectedCallback() {
+        getCustomSettingsWeather()
+        .then(result=>{
+            this.api = result.Forecast_Service_URL__c;
+            this.apiKey = result.Api_Key__c;
+        })
+        .catch(error=>{
+            console.log('=====',error);
+        })
+    }
+
     weatherCheck() {
-        this.forecast = true;
+        // this.forecast = true;
+        getTemperature({api: this.api, apiKey: this.apiKey, city: this.city})
+        .then(data =>{
+            console.log('====== Data', data);
+        })
+        .catch(error =>{
+            console.log('=====errror',error);
+        })
     }
 
     onchangeInputCity(event) {
-      
+        this.city = event.target.value;
     }
 }
