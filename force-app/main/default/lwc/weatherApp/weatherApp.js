d3d6d68a0d2894252193e58d6a0ba04ac49f710f
yury trypolski <yury101.89@gmail.com>
2022-01-04 20:14:55 +0300

    output content in ui

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -3,12 +3,43 @@ import getCustomSettingsWeather from '@salesforce/apex/WeatherAppController.getC
 
 export default class WeatherApp extends LightningElement {
     city;
+    
+    dateForecast;
+    timeForecast;
+    temperatureForecast;
 
     onchangeInputCity(event) {
         this.city = event.target.value;
     }
 
     weatherCheck() {
-        getCustomSettingsWeather({city: this.city});
+        getCustomSettingsWeather({city: this.city})
+        .then(result => {
+            const arrayDate = [];
+            const arrayTime = [];
+            const arrayTemp = [];
+            result.listForecasts.forEach(item=>{
+                const currentDate = new Date(item.dt * 1000);
+                const currentDayOfMonth = currentDate.getDate();
+                const currentMonth = currentDate.getMonth(); 
+                const currentYear = currentDate.getFullYear();
+                const dateForecast = currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
+                arrayDate.push(dateForecast);
+                this.dateForecast = arrayDate;
+
+                const currentHour = currentDate.getHours();
+                const currentMinutes = currentDate.getMinutes();
+                const timeForecast = currentHour + ":" + currentMinutes;
+                arrayTime.push(timeForecast);
+                this.timeForecast = arrayTime;
+
+                const temperatureForecast = item.main.temp;
+                arrayTemp.push(temperatureForecast);
+                this.temperatureForecast = arrayTemp;
+            })
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
 }
