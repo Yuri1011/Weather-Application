5b208351b3a3bf11e0717d9256196a16a1feba4b
yury trypolski <yury101.89@gmail.com>
2022-01-07 04:45:11 +0300

    refactor code

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -3,6 +3,10 @@ import getData from '@salesforce/apex/WeatherAppController.getData';
 
 export default class WeatherApp extends LightningElement {
     city;
+    dateForecast;
+    timeForecast;
+    temperatureForecast;
+
     forecastRecords;
 
     onchangeInputCity(event) {
@@ -13,6 +17,29 @@ export default class WeatherApp extends LightningElement {
         getData({city: this.city})
         .then(result => {
             this.forecastRecords = result.listForecasts;
+            // console.log('====>>> ', this.fore);
+            // const arrayDate = [];
+            // const arrayTime = [];
+            // const arrayTemp = [];
+            // result.listForecasts.forEach(item=>{
+            //     const currentDate = new Date(item.dt * 1000);
+            //     const currentDayOfMonth = currentDate.getDate();
+            //     const currentMonth = currentDate.getMonth(); 
+            //     const currentYear = currentDate.getFullYear();
+            //     const dateForecast = currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
+            //     arrayDate.push(dateForecast);
+            //     this.dateForecast = arrayDate;
+
+            //     const currentHour = currentDate.getHours();
+            //     const currentMinutes = currentDate.getMinutes();
+            //     const timeForecast = currentHour + ":" + currentMinutes;
+            //     arrayTime.push(timeForecast);
+            //     this.timeForecast = arrayTime;
+
+            //     const temperatureForecast = item.main.temp;
+            //     arrayTemp.push(temperatureForecast);
+            //     this.temperatureForecast = arrayTemp;
+            // })
         })
         .catch(error =>{
             console.log(error);
